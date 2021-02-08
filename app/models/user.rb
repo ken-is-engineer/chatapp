1c09f64059e544fe70641941e0856a3e455a0cbc
ken-is-engineer <kennoadoresu03@gmail.com>
2021-02-08 18:55:52 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  has_many :users, through: :room_users
   validates :name, presence: true
 end
