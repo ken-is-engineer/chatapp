fe2b803f20a85432081697580ebb44f619c7c1c1
ken-is-engineer <kennoadoresu03@gmail.com>
2021-02-09 16:55:14 +0900

    メッセージ表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,16 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
-    @message = @room.message.new(message_params)
+    @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user) #コントローラを通さずにindex.html.erbにアクセスするので、@messagesを定義しておかないとエラーになる。
       render :index
     end
   end
