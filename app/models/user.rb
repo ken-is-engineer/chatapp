4f897bc19317909558046cba54ffd99d02d2814e
ken-is-engineer <kennoadoresu03@gmail.com>
2021-02-09 11:30:15 +0900

    チャットルーム作成機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :users, through: :room_users
+
   validates :name, presence: true
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
